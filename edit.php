608f45605c597273e7ee208550c0f53b8b2b2e1b
barrygg11 <barry01test@gmail.com>
2021-04-07 08:32:24 +0800

    金額新增功能畫面正常

--- edit.php
+++ edit.php
@@ -8,14 +8,17 @@ include ('database.php');
         <title>編輯會員資料系統</title>
     </head>
     <body>
+        
     <?php
         $account = $_SESSION['account'];
+        $data = new db;
+        $lobby = $data -> dataUser($account);
     ?>
         <form name="edit" action="lobby.php" method="post">
-        會員名稱 : <input type="text" name="username"><p>
-        手機 : <input type="text" name="phone"><p>
+        會員名稱 : <input type="text" name="username" value="<?php echo($lobby['username']) ?>"><p>
+        手機 : <input type="text" name="phone" value="<?php echo($lobby['phone']) ?>"><p>
         帳號 : <input type="account" name="account" value="<?php echo $account; ?>" disabled><p>
-        密碼 : <input type="password" name="password"><p>
+        密碼 : <input type="password" name="password" value="<?php echo($lobby['password']) ?>"><p>
         <input type="submit" name="btn" value="修改">
         <input type="button" onclick="history.back()" value="回上頁"></input>
         </form>
